e5a8581202795c60b3865ce2feebcfbdc28177eb
wojtek <wojtek@wojtek-PC>
2016-10-18 18:38:53 +0200

    remove imports

--- kovalevsky-web/src/main/java/kovalevsky/imaging/DataProcessor.java
+++ kovalevsky-web/src/main/java/kovalevsky/imaging/DataProcessor.java
@@ -12,8 +12,6 @@ package kovalevsky.imaging;
 import java.util.ArrayList;
 import java.util.List;
 
-import kovalevsky.imaging.formats.ImageFormat;
-
 /**
  * Provides all algorithms implemented by Kovalewsky.
  * 
